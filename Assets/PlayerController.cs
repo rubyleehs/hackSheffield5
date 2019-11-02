ac868b38579da57606cba323fa10512bc0b32b5d
nadaward <nadoedodo@hotmail.com>
2019-11-02 15:27:36 +0000

    playercontroller

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
-    public Transform squareTrasform;
-
+    public Transform squareTransform;
+    
+    private bool isRunning;
+    private int sprintCount;
     private Vector3 playerInput;
+    private Vector3 maxPlayerInput;
     private new Transform transform;
     private SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
+        //isRunning = false;
+        sprintCount = 100;
         transform = this.GetComponent<Transform>();
         spriteRenderer = this.GetComponent<SpriteRenderer>();
     }
@@ -21,9 +26,31 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerInput = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        playerInput = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        if (Input.GetKey(KeyCode.LeftShift) && (sprintCount <= 100 && sprintCount > 0)){
+            //isRunning = true;
+            moveSpeed = 10;
+            transform.position += playerInput * moveSpeed * Time.deltaTime;
+            sprintCount -= 1;
+        }
+        else if (sprintCount < 100 && !Input.GetKey(KeyCode.LeftShift)){ 
+            sprintCount += 1;
+            moveSpeed = 5;
+            transform.position += playerInput * moveSpeed * Time.deltaTime;
+        }
+        else{
+            moveSpeed = 5;
+            transform.position += playerInput * moveSpeed * Time.deltaTime;
+            }
+
+
         transform.position += playerInput * moveSpeed * Time.deltaTime;
         spriteRenderer.color = new Color(transform.position.x, transform.position.y, 50);
-        squareTrasform.GetComponent<SpriteRenderer>().color = new Color(transform.position.x, transform.position.y, 50);
+        squareTransform.GetComponent<SpriteRenderer>().color = new Color(transform.position.x, transform.position.y, 50);
+        
+        
+        
+
+        
     }
 }
