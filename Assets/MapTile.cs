abe096c38c5587f642d8ef0799e801373e569fee
rubyleehs <rubyleehs@live.com>
2019-11-02 15:28:16 +0000

    MapManagerUnfinished

---
+++ Assets/MapTile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapTile : ScriptableObject
+{
+    public int id;
+    public bool isSolid;
+
+    public Sprite sprite;
+
+}
