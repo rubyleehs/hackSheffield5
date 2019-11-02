e6724d3c20700ebee2ce265b056f562be0e76987
rubyleehs <rubyleehs@live.com>
2019-11-02 12:47:35 +0000

    Moving House added
    kreuihcw

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Transform squareTrasform;
+
+    private Vector3 playerInput;
+    private new Transform transform;
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = this.GetComponent<Transform>();
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerInput = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        transform.position += playerInput * moveSpeed * Time.deltaTime;
+        spriteRenderer.color = new Color(transform.position.x, transform.position.y, 50);
+        squareTrasform.GetComponent<SpriteRenderer>().color = new Color(transform.position.x, transform.position.y, 50);
+    }
+}
